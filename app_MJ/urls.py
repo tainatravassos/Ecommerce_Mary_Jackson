f4e0fca9c48fd2849c397046ae1de1b0a70f98c4
luanaxcardoso <luanacardoso.bc@gmail.com>
2024-03-24 15:07:43 -0300

    feat: adicionando url - sobre

--- app_MJ/urls.py
+++ app_MJ/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from app_MJ.views import base
+from app_MJ.views import base, sobre
 
 urlpatterns = [
     path('', base, name='base'),
+    path('sobre/', sobre, name="sobre")
 
 ]
