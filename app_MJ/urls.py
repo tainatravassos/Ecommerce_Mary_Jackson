43a691319f283a107e453090d3f05f0deb65f1b1
luanaxcardoso <luanacardoso.bc@gmail.com>
2024-03-24 15:40:00 -0300

    feat: atualização das urls

--- app_MJ/urls.py
+++ app_MJ/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from app_MJ.views import base, sobre
+from app_MJ.views import base, sobre, contato
 
 urlpatterns = [
     path('', base, name='base'),
-    path('sobre/', sobre, name="sobre")
+    path('sobre/', sobre, name="sobre"),
+    path('contato/', contato, name="contato")
 
 ]
