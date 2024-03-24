e30f51aa5e31648c660243acd5a68ca9fee35b80
luanaxcardoso <luanacardoso.bc@gmail.com>
2024-03-24 20:54:06 -0300

    criação das models

--- app_MJ/models.py
+++ app_MJ/models.py
@@ -1,14 +1,90 @@
 from django.db import models
 from datetime import datetime
+from django.contrib.auth.models import User
+
+class Clientes(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    nome_completo = models.CharField(max_length=200)
+    endereco = models.CharField(max_length=200, null=True, blank=True)
+    cidade = models.CharField(max_length=200)
+    estado = models.CharField(max_length=200)
+    data_cadastro = models.DateTimeField(default=datetime.now, blank=True)
+    telefone = models.CharField(max_length=200)
+    email = models.EmailField(max_length=200, null=True, blank=True, unique=True)
+
+    def __str__(self):
+        return self.nome_completo
 
 
-class Produto(models.Model):
-    nome = models.CharField(max_length=100)
-    preco = models.DecimalField(max_digits=5, decimal_places=2)
-    descricao = models.TextField()
-    quantidade = models.IntegerField()
-    foto = models.ImageField(upload_to="produtos", null=True, blank=True)
-    data_produto = models.DateTimeField(default=datetime.now, blank=True)
+class Categoria(models.Model):
+    nome = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200, unique=True)
 
     def __str__(self):
         return self.nome
+
+
+class Produto(models.Model):
+    nome_produto = models.CharField(max_length=200, default='Produto sem nome')
+    slug = models.SlugField(max_length=200, default='')
+    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE, default=1) 
+    descricao = models.TextField()
+    quantidade = models.IntegerField(default=0)
+    imagem = models.ImageField(upload_to="", null=True, blank=True)
+    preco = models.DecimalField(max_digits=5, decimal_places=2)
+    visualizacao = models.IntegerField(default=0)
+    data_produto = models.DateTimeField(default=datetime.now, blank=True)
+    
+    def __str__(self):
+        return self.nome_produto
+
+
+class Carrinho(models.Model):
+    cliente = models.ForeignKey(Clientes, on_delete=models.CASCADE)
+    total = models.DecimalField(default=0, max_digits=5, decimal_places=2)
+    data_pedido = models.DateTimeField(default=datetime.now, blank=True)
+    
+
+    def __str__(self):
+        return f"Carrinho ID: {self.id} - Cliente: {self.cliente.nome_completo}"
+    
+
+class CarrinhoProduto(models.Model):
+    carrinho = models.ForeignKey(Carrinho, on_delete=models.CASCADE)
+    produto = models.ForeignKey(Produto, on_delete=models.CASCADE)
+    quantidade = models.IntegerField()
+    preco = models.DecimalField(max_digits=5, decimal_places=2)
+    avaliacao = models.PositiveIntegerField()
+    subtotal = models.PositiveIntegerField()
+    data_pedido = models.DateTimeField(default=datetime.now, blank=True)
+    
+
+    def __str__(self):
+        return f"Carrinho: {self.carrinho.id} - CarrinhoProduto: {self.id} - Produto: {self.produto.nome_produto}"
+
+PEDIDO_STATUS = (
+    ('Pedido Recebido', 'Pedido Recebido'),
+    ('Pedido em Processamento', 'Pedido em Processamento'),
+    ('Pedido Enviado', 'Pedido Enviado'),
+    ('Pedido Entregue', 'Pedido Entregue'),
+    ('Pedido Cancelado', 'Pedido Cancelado'),
+)
+
+
+class Pedido_order(models.Model):
+    carrinho = models.ForeignKey(Carrinho, on_delete=models.CASCADE)
+    ordenando_por = models.CharField(max_length=200)
+    endereco_envio = models.CharField(max_length=200)
+    cliente = models.ForeignKey(Clientes, on_delete=models.CASCADE)
+    subtotal = models.PositiveIntegerField()
+    total = models.PositiveIntegerField()
+    data_pedido = models.DateTimeField(default=datetime.now, blank=True)
+    desconto = models.DecimalField(default=0, max_digits=5, decimal_places=2)
+    telefone = models.CharField(max_length=200)
+    email = models.EmailField(max_length=200, null=True, blank=True)
+    pedido_status = models.CharField(max_length=200, choices=PEDIDO_STATUS, default='Pedido Recebido')
+    observacoes = models.TextField(null=True, blank=True)
+    
+
+    def __str__(self):
+        return f"Pedido_order: {self.id} - Cliente: {self.cliente.nome_completo} - Status: {self.pedido_status}"
