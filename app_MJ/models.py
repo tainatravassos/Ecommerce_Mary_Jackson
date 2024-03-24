5e193b8bf0b3392bf7549c0c2601cd3aa5634809
luanaxcardoso <luanacardoso.bc@gmail.com>
2024-03-23 23:53:05 -0300

    inico views

--- app_MJ/models.py
+++ app_MJ/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Produto(models.Model):
+    nome = models.CharField(max_length=100)
+    preco = models.DecimalField(max_digits=5, decimal_places=2)
+    descricao = models.TextField()
+    foto = models.ImageField(upload_to='produtos', null=True, blank=True)
+    def __str__(self):
+        return self.nome
