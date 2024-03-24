4ec39aabdc52368b88c1253193a731dac2d6b407
luanaxcardoso <luanacardoso.bc@gmail.com>
2024-03-24 15:10:38 -0300

    feat: Adicionando class produtos

--- app_MJ/models.py
+++ app_MJ/models.py
@@ -1,10 +1,14 @@
 from django.db import models
+from datetime import datetime
 
-# Create your models here.
+
 class Produto(models.Model):
     nome = models.CharField(max_length=100)
     preco = models.DecimalField(max_digits=5, decimal_places=2)
     descricao = models.TextField()
-    foto = models.ImageField(upload_to='produtos', null=True, blank=True)
+    quantidade = models.IntegerField()
+    foto = models.ImageField(upload_to="produtos", null=True, blank=True)
+    data_produto = models.DateTimeField(default=datetime.now, blank=True)
+
     def __str__(self):
         return self.nome
