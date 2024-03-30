52813deb4871553178e1447f540ee963c41677ec
LuciBustamante <luci1bust@gmail.com>
2024-03-29 21:18:54 -0300

    feat: adjust css file and create router of cadastrar produto

--- app_MJ/urls.py
+++ app_MJ/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from app_MJ.views import home, sobre, contato
+from app_MJ.views import home, sobre, contato, cadastro_produto
 
 urlpatterns = [
     path('', home, name='home'),
     path('sobre/', sobre, name="sobre"),
-    path('contato/', contato, name="contato")
-
+    path('contato/', contato, name="contato"),
+    path('cadastro-produto/', cadastro_produto, name="cadastro_produto")
 ]
--- app_MJ/views.py
+++ app_MJ/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from app_MJ.models import *
 
+
 def home(request):
     produtos = Produto.objects.all()
     return render(request, 'home.html', {'produtos': produtos})
@@ -10,3 +11,6 @@ def sobre(request):
 
 def contato(request):
     return render(request, "contato.html", {'contato': 'active'})
+
+def cadastro_produto(request):
+    return render(request, "cadastro_produto.html", {'cadastro_produto': 'active'})
