d0f12df9cfe5278387a6cbaece5a58788beedae1
Stesilva16 <stefany.silva2020.16@gmail.com>
2024-04-07 21:24:57 -0300

    instalação

--- settings.py
+++ settings.py
@@ -1,4 +1,7 @@
 # settings.py
 
-# Adicione o host permitido
-ALLOWED_HOSTS = ['maryjackson-e5a137c3c635.herokuapp.com']
+ALLOWED_HOSTS = [
+    'localhost',  # Adicione o host local, se necessário
+    '127.0.0.1',  # Adicione o host local, se necessário
+    'maryjackson-e5a137c3c635.herokuapp.com',
+]
