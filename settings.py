0ab2b875332361cf56d8232ecdd5e21afa9762cb
Stesilva16 <stefany.silva2020.16@gmail.com>
2024-04-08 21:11:20 -0300

    teste de deplyed

--- settings.py
+++ settings.py
@@ -1,5 +1,5 @@
 ALLOWED_HOSTS = [
-    'maryjackson.herokuapp.com',
-    'maryjackson-e5a137c3c635.herokuapp.com',  # Adicione este host
-    'localhost'
+    'maryjackson.herokuapp.com',  # Host original do Heroku
+    'localhost',                   # Host local para desenvolvimento
+    'maryjackson-e5a137c3c635.herokuapp.com',  # Novo host do Heroku
 ]
