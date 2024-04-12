cf4a3360c3fa0fb4650a8916625d1cd2823fabeb
ANA CAROLINA TORCHIA DE SOUZA <95150688+caroltorchia@users.noreply.github.com>
2024-04-12 00:05:06 -0300

    adicionando teste de models

--- app_MJ/models.py
+++ app_MJ/models.py
@@ -1,18 +1,47 @@
-from django.db import models
-from datetime import datetime
+import datetime
+
 from django import forms
 from django.contrib.auth.models import User
+from django.db import models
+from django.utils import timezone
+
 
 class Cliente(models.Model):
     estados = (
-        ('AC', 'AC'), ('AL', 'AL'), ('AP', 'AP'), ('AM', 'AM'), ('BA', 'BA'), ('CE', 'CE'), ('ES', 'ES'), ('GO', 'GO'), ('MA', 'MA'), ('MT', 'MT'), ('MS', 'MS'), ('MG', 'MG'), ('PA', 'PA'), ('PB', 'PB'), ('PR', 'PR'), ('PE', 'PE'), ('PI', 'PI'), ('RJ', 'RJ'), ('RN', 'RN'), ('RS', 'RS'), ('RO', 'RO'), ('RR', 'RR'), ('SC', 'SC'), ('SP', 'SP'), ('SE', 'SE'), ('TO', 'TO'), ('DF', 'DF'),    
+        ("AC", "AC"),
+        ("AL", "AL"),
+        ("AP", "AP"),
+        ("AM", "AM"),
+        ("BA", "BA"),
+        ("CE", "CE"),
+        ("ES", "ES"),
+        ("GO", "GO"),
+        ("MA", "MA"),
+        ("MT", "MT"),
+        ("MS", "MS"),
+        ("MG", "MG"),
+        ("PA", "PA"),
+        ("PB", "PB"),
+        ("PR", "PR"),
+        ("PE", "PE"),
+        ("PI", "PI"),
+        ("RJ", "RJ"),
+        ("RN", "RN"),
+        ("RS", "RS"),
+        ("RO", "RO"),
+        ("RR", "RR"),
+        ("SC", "SC"),
+        ("SP", "SP"),
+        ("SE", "SE"),
+        ("TO", "TO"),
+        ("DF", "DF"),
     )
 
     nome_completo = models.CharField(max_length=200)
     endereco = models.CharField(max_length=200, null=True, blank=True)
     cidade = models.CharField(max_length=100)
     estado = models.CharField(max_length=4, choices=estados)
-    data_cadastro = models.DateTimeField(default=datetime.now, blank=True)
+    data_cadastro = models.DateTimeField(default=timezone.now, blank=True)
     telefone = models.CharField(max_length=200)
     email = models.EmailField(max_length=200, null=True, blank=True, unique=True)
     senha = models.CharField(max_length=15)
@@ -30,25 +59,29 @@ class Categoria(models.Model):
 
 
 class Produto(models.Model):
-    nome_produto = models.CharField(max_length=200, default='')
-    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE, default=1) 
+    nome_produto = models.CharField(max_length=200, default="")
+    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE, default=1)
     descricao = models.TextField()
     quantidade = models.IntegerField(default=0)
     imagem = models.ImageField(upload_to="", null=True, blank=True)
     preco = models.DecimalField(max_digits=5, decimal_places=2)
-    data_produto = models.DateTimeField(default=datetime.now, blank=True)
-    
+    data_produto = models.DateTimeField(default=timezone.now, blank=True)
+
     def __str__(self):
         return self.nome_produto
-    
+
     def imagem_url(self):
         if self.imagem:
             return self.imagem.url
         else:
             return None
+
+
 class Carrinho(models.Model):
-    cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE, null=True, blank=True)
-    data_pedido = models.DateTimeField(default=datetime.now, blank=True)
+    cliente = models.ForeignKey(
+        Cliente, on_delete=models.CASCADE, null=True, blank=True
+    )
+    data_pedido = models.DateTimeField(default=timezone.now, blank=True)
 
     def __str__(self):
         if self.cliente:
@@ -62,11 +95,7 @@ class CarrinhoProduto(models.Model):
     produto = models.ForeignKey(Produto, on_delete=models.CASCADE)
     quantidade = models.IntegerField()
     preco = models.DecimalField(max_digits=5, decimal_places=2)
-    data_pedido = models.DateTimeField(default=datetime.now, blank=True)
-    
+    data_pedido = models.DateTimeField(default=timezone.now, blank=True)
+
     def __str__(self):
         return f"Carrinho: {self.carrinho.id} - CarrinhoProduto: {self.id} - Produto: {self.produto.nome_produto}"
-
-
-
-
--- app_MJ/tests.py
+++ app_MJ/tests.py
@@ -1,3 +1,124 @@
 from django.test import TestCase
+from app_MJ.models import Cliente, Categoria, Produto, Carrinho, CarrinhoProduto
 
-# Create your tests here.
+
+class ClienteModelTest(TestCase):
+    """
+    Test cases para o model Cliente
+    """
+
+    def setUp(self):
+        Cliente.objects.create(
+            nome_completo="John Doe",
+            endereco="123 Main St",
+            cidade="Anytown",
+            estado="NY",
+            telefone="123-456-7890",
+            email="john.doe@example.com",
+            senha="password123",
+        )
+
+    def test_creation(self):
+        """Test da criação do model"""
+        cliente = Cliente.objects.first()
+        self.assertEqual(cliente.nome_completo, "John Doe")
+        self.assertEqual(cliente.endereco, "123 Main St")
+        self.assertEqual(cliente.cidade, "Anytown")
+        self.assertEqual(cliente.estado, "NY")
+        self.assertEqual(cliente.telefone, "123-456-7890")
+        self.assertEqual(cliente.email, "john.doe@example.com")
+        self.assertEqual(cliente.senha, "password123")
+
+    def test_str_representation(self):
+        """Test do metodo str da classe"""
+        cliente = Cliente.objects.first()
+        self.assertEqual(str(cliente), cliente.nome_completo)
+
+
+class CategoriaModelTest(TestCase):
+    def setUp(self):
+        Categoria.objects.create(nome="Electronics", slug="electronics")
+
+    def test_creation(self):
+        categoria = Categoria.objects.first()
+        self.assertEqual(categoria.nome, "Electronics")
+        self.assertEqual(categoria.slug, "electronics")
+
+    def test_str_representation(self):
+        categoria = Categoria.objects.first()
+        self.assertEqual(str(categoria), categoria.nome)
+
+
+class ProdutoModelTest(TestCase):
+    def setUp(self):
+        categoria = Categoria.objects.create(nome="Electronics", slug="electronics")
+        Produto.objects.create(
+            nome_produto="Smartphone",
+            categoria=categoria,
+            descricao="A high-quality smartphone.",
+            quantidade=10,
+            preco=99.99,
+        )
+
+    def test_creation(self):
+        produto = Produto.objects.first()
+        self.assertEqual(produto.nome_produto, "Smartphone")
+        self.assertEqual(produto.categoria.nome, "Electronics")
+        self.assertEqual(produto.descricao, "A high-quality smartphone.")
+        self.assertEqual(produto.quantidade, 10)
+        self.assertEqual(float(produto.preco), 99.99)
+
+    def test_str_representation(self):
+        produto = Produto.objects.first()
+        self.assertEqual(str(produto), produto.nome_produto)
+
+
+class CarrinhoModelTest(TestCase):
+    def setUp(self):
+        cliente = Cliente.objects.create(
+            nome_completo="John Doe",
+            endereco="123 Main St",
+            cidade="Anytown",
+            estado="NY",
+            telefone="123-456-7890",
+            email="john.doe@example.com",
+            senha="password123",
+        )
+        Carrinho.objects.create(cliente=cliente)
+
+    def test_creation(self):
+        carrinho = Carrinho.objects.first()
+        self.assertIsNotNone(carrinho.cliente)
+        self.assertEqual(
+            str(carrinho),
+            f"Carrinho ID: {carrinho.id} - Cliente: {carrinho.cliente.nome_completo}",
+        )
+
+
+class CarrinhoProdutoModelTest(TestCase):
+    def setUp(self):
+        carrinho = Carrinho.objects.create()
+        produto = Produto.objects.create(
+            nome_produto="Smartphone",
+            categoria=Categoria.objects.create(nome="Electronics", slug="electronics"),
+            descricao="A high-quality smartphone.",
+            quantidade=10,
+            preco=99.99,
+        )
+        CarrinhoProduto.objects.create(
+            carrinho=carrinho, produto=produto, quantidade=1, preco=99.99
+        )
+
+    def test_creation(self):
+        carrinho_produto = CarrinhoProduto.objects.first()
+        self.assertIsNotNone(carrinho_produto.carrinho)
+        self.assertIsNotNone(carrinho_produto.produto)
+        self.assertEqual(carrinho_produto.quantidade, 1)
+        self.assertEqual(float(carrinho_produto.preco), 99.99)
+
+    def test_str_representation(self):
+        carrinho_produto = CarrinhoProduto.objects.first()
+        self.assertEqual(
+            str(carrinho_produto),
+            f"Carrinho: {carrinho_produto.carrinho.id} - CarrinhoProduto: {carrinho_produto.id} - Produto: {carrinho_produto.produto.nome_produto}",
+        )
